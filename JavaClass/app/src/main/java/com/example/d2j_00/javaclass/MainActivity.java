881c4f5b271570e7d1780568727ed05fc37ef154
antonprasetyautomo <antonprasetyautomo78@gmail.com>
2017-01-31 10:22:23 +0700

    Java Class ditambahkan

--- JavaClass/app/src/main/java/com/example/d2j_00/javaclass/MainActivity.java
+++ JavaClass/app/src/main/java/com/example/d2j_00/javaclass/MainActivity.java
@@ -2,12 +2,43 @@ package com.example.d2j_00.javaclass;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public String showString;
+    public TextView showText;
+    public barang [] arrBarang = new barang[10];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        showText = (TextView) findViewById(R.id.show_text);
+
+        showString = "Manipulasi class java android";
+        addSeparate();
+
+        initBarang();
+        showString+= arrBarang[5].toString();
+        addSeparate();
+        showString += arrBarang[1].toString();
+        showText.setText(showString);
+    }
+
+    public void addSeparate(){
+        showString+= "\n-----------------------\n";
+    }
+
+    public void initBarang(){
+        arrBarang[0]= new barang("laptop", 1, 7000000);
+        arrBarang[1]= new barang("laptop", barang.ELEKTRONIK, 2500000);
+        arrBarang[2]= new barang("Monitor", barang.ELEKTRONIK, 1500000);
+        arrBarang[3]= new barang("Scanner", barang.ELEKTRONIK, 1000000);
+        arrBarang[4]= new barang("Meja",barang.Non_ELECTRONIK, 700000);
+        arrBarang[5]= new barang("Mouse", barang.ELEKTRONIK, 200000);
+        arrBarang[6]= new barang("Kursi", barang.Non_ELECTRONIK, 2000000);
+        arrBarang[7]= new barang("RAM", barang.ELEKTRONIK, 1000000);
+        arrBarang[8]= new barang("Almari", barang.Non_ELECTRONIK, 10000000);
+        arrBarang[9]= new barang("Pintu", barang.Non_ELECTRONIK, 15000000);
     }
 }
---
+++ JavaClass/app/src/main/java/com/example/d2j_00/javaclass/barang.java
@@ -0,0 +1,58 @@
+package com.example.d2j_00.javaclass;
+
+import java.util.jar.Attributes;
+
+/**
+ * Created by D2J-00 on 31/01/2017.
+ */
+public class barang {
+    public static final int ELEKTRONIK = 1;
+    public static final int Non_ELECTRONIK = 2;
+    private String nama;
+    private int category;
+    private int harga;
+
+    public barang(String nama, int category, int harga) {
+        this.nama = nama;
+        this.setCategory(category);
+        this.harga = harga;
+    }
+
+    public String toString(){
+        return nama+" | "+this.getStringCategory()+" | "+this.harga+"\n";
+    }
+
+    public void setNama(String nama){
+        this.nama = nama;
+    }
+
+    public String getNama(){
+        return this.nama;
+    }
+
+    public int getCategory() {
+        return category;
+    }
+
+    public String getStringCategory(){
+        if (category == 1)
+            return "Elektronik";
+        else
+            return "Non Elektronik";
+    }
+
+    public void setCategory(int category) {
+        if (category !=1 && category != 2)
+            this.category=2;
+        else
+            this.category = category;
+    }
+
+    public int getHarga() {
+        return harga;
+    }
+
+    public void setHarga(int harga) {
+        this.harga = harga;
+    }
+}
